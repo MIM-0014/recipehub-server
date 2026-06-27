0f47ddfc45d7302431999216f56e81afdb344fc5
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-06-27 13:24:47 +0600

    src/config/db.js

---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmalja2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+
+    console.log("✅ MongoDB Connected Successfully");
+
+    return client.db("recipeHubDB");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed");
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
