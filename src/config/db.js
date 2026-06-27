360c5e11e171b0c929caee5cf91140215178604b
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-06-27 22:01:25 +0600

    add serviceAccountKey.json

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmalja2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASS)}@cluster0.dmalja2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
